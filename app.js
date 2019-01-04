ca5966aeeee611606e2fdae148a9960fa445ff74
donjoe <giumontanaro@gmail.com>
2019-01-04 16:35:42 +0100

    Filter fonts array with regExp ignoring case, and display list of fonts

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const fontSearch = document.getElementById('font_input');
-const list = document.getElementById('font_list')
+const list = document.getElementById('font_list');
+const fontLink = document.getElementById('font_link');
+
 const fonts = [];
 
 //Request google fonts with axios
 document.addEventListener('DOMContentLoaded', () => {
 
     axios.get('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyArDywurj-c4U_vGDUXbOd-oViu9yMHo10')
-    .then(res => res.data)
-    .then(data => {
-        fonts.push(...data.items);
-    });
-    
+        .then(res => res.data)
+        .then(data => {
+            fonts.push(...data.items);
+        });
+
 });
 
 function filterFonts(e) {
-    /*
-    startsWith is case sensitive, so normalize the case of family before filtering
-    */
-    const filtered = fonts.map(f => f.family.toLowerCase() )
-    .filter(family => family.startsWith(e.target.value.toLowerCase()) );
-
-    displayFonts(filtered);
+    if (e.target.value.length > 0) {
+        const regEx = new RegExp('^' + e.target.value, 'i'); //test if starts with, ignoring case
+        const filtered = fonts.filter(({ family }) => regEx.test(family));
+        displayFonts(filtered);
+    }
 }
 
 function displayFonts(fonts) {
-    list.innerHTML = fonts.map(f => `<li class="font_item">${f}</li>`).join("");
+    list.innerHTML = fonts.map(({ family, variants, subsets }) => `<li class="font_item" data-family="${family}" data-variants="${variants}" data-subsets="${subsets}">${family}</li>`).join("");
 }
 
 fontSearch.addEventListener('input', filterFonts);
